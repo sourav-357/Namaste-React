58e2287b4c2c2758558f36e2629d457262f2fc65
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-06 07:45:33 +0530

    child under child

--- Episode-01-Inception/script.js
+++ Episode-01-Inception/script.js
@@ -2,10 +2,21 @@
 
 // First react code 
 
-const heading = React.createElement("h1", {id: "heading"}, "hello from react code");
+// const heading = React.createElement("h1", {id: "heading"}, "hello from react code");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// putting elements under elements 
+
+const parent = React.createElement("div", { id: "parent" }, 
+    React.createElement("div", { id: "child" }, 
+        React.createElement("h1", { id: "heading" }, "This is the main heading tag" )
+    )
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+console.log(parent);
+root.render(parent);
 
 
 // 
-
