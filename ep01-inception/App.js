c4f465ccafe7ecf6771b1bef5c46e5abd237c1f3
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-11 07:07:26 +0530

    Updated folder structure

---
+++ ep01-inception/App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag"),
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag"),
+    ]),
+]);
+
+console.log(parent);
+
+ReactDOM.createRoot(document.getElementById("root")).render(parent);
---
+++ ep02-igniting-our-app/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag"),
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag"),
+    ]),
+]);
+
+console.log(parent);
+
+ReactDOM.createRoot(document.getElementById("root")).render(parent);
